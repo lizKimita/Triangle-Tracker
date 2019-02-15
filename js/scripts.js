4fb5231541cb383eedc51c4483a2a96041266a04
Elizabeth Kimita <kimita.wanjiku@gmail.com>
2019-02-15 12:17:17 +0300

    Adding the changes to var AB and c

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,5 @@ var triangle = function ( sideA, sideB, sideC );
 var sideA = parseFloat(prompt("Enter the length value of side A here:"));
 var sideB = parseFloat(prompt("Enter the length value of side B here"));
 var sideC = parseFloat(prompt("Enter the length value of side C here"));
+function return [];
+if 
