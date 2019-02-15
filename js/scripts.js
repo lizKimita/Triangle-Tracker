7d56b11accc864a0814174efefe112fda1c05a3f
Elizabeth Kimita <kimita.wanjiku@gmail.com>
2019-02-15 12:05:44 +0300

    adding the change in page style to alerts

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var triangle = function ( sideA, sideB, sideC );
+var sideA = parseFloat(prompt("Enter the length value of side A here:"));
+var sideB = parseFloat(prompt("Enter the length value of side B here"));
+var sideC = parseFloat(prompt("Enter the length value of side C here"));
