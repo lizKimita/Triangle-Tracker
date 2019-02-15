8b6deedca0c772cd313c8e13747b381be648538f
Elizabeth Kimita <kimita.wanjiku@gmail.com>
2019-02-15 18:55:38 +0300

    adding css style changes

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ var sideB = parseFloat(prompt("Enter the length value of side B here"));
 var sideC = parseFloat(prompt("Enter the length value of side C here"));
 //BUSINESS LOGICS - Backend
 if(sideA <= 0 || sideB <= 0 || sideC <= 0){
-    alert ("Invalid entry, Please enter a value above 0");
+    alert ("Invalid entry, Please enter a value above 0!");
 } else{if (sideA===sideB && sideB===sideC && sideA===sideC) {
   alert ("This triangle is an equilateral triangle");
 }
@@ -17,6 +17,6 @@ else if (sideA===sideB || sideB===sideC || sideA===sideC) {
 else if (sideA + sideB <= sideC || sideB + sideC<=sideA || sideA + sideC<=sideB) {
   alert ("This is a not a triangle!");
 }
-else alert("Unrecognised character,Please enter a valid number");
+else alert("Unrecognised character,Please enter a valid number!");
 }
 };
