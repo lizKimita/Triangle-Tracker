d9f08d5a414c2ff3c13931b0eebf320ee79f837b
Elizabeth Kimita <kimita.wanjiku@gmail.com>
2019-02-15 12:52:47 +0300

    adding isosceles and not a triangle option

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,16 @@ var triangle = function ( sideA, sideB, sideC );
 var sideA = parseFloat(prompt("Enter the length value of side A here:"));
 var sideB = parseFloat(prompt("Enter the length value of side B here"));
 var sideC = parseFloat(prompt("Enter the length value of side C here"));
-var return [];
+function return [];
 
 if (sideA===sideB && sideB===sideC && sideA===sideC) {
-  alert ("This triangle is an equilateral triangle")};
+  alert ("This triangle is an equilateral triangle");
+};
+else if (sideA===sideB || sideB===sideC || sideA===sideC) {
+  alert ("This triangle is an isosceles triangle");
+};
+else if (sideA + sideB <= sideC || sideB + sideC<=sideA || sideA + sideC<=sideB) {
+  alert ("This is a not a triangle!")
+}
+else {sideA + sideB > sideC || sideB + sideC>sideA || sideA + sideC>sideB)
+  alert ("This is a scalene")
