b4a9605d01200885e75cd2e727eb8aab28ab8089
Elizabeth Kimita <kimita.wanjiku@gmail.com>
2019-02-15 15:02:50 +0300

    adding user interface and business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 function triangle() {
+//USER INTERFACE
 var sideA = parseFloat(prompt("Enter the length value of side A here:"));
 var sideB = parseFloat(prompt("Enter the length value of side B here"));
 var sideC = parseFloat(prompt("Enter the length value of side C here"));
-
+//BUSINESS LOGICS
 if (sideA===sideB && sideB===sideC && sideA===sideC) {
   alert ("This triangle is an equilateral triangle");
 }
