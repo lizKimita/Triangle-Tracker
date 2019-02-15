00fec1fe82a292f54e55fc643eb268f6570bea0c
Elizabeth Kimita <kimita.wanjiku@gmail.com>
2019-02-15 12:26:29 +0300

    Adding the equilateral triangle option

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,7 @@ var triangle = function ( sideA, sideB, sideC );
 var sideA = parseFloat(prompt("Enter the length value of side A here:"));
 var sideB = parseFloat(prompt("Enter the length value of side B here"));
 var sideC = parseFloat(prompt("Enter the length value of side C here"));
-function return [];
-if 
+var return [];
+
+if (sideA===sideB && sideB===sideC && sideA===sideC) {
+  alert ("This triangle is an equilateral triangle")};
