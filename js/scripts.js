7b42411758e5412f712a7e2fc85ca5de413c9472
Elizabeth Kimita <kimita.wanjiku@gmail.com>
2019-02-15 14:16:13 +0300

    adding working button

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,16 @@
-var triangle = function ( sideA, sideB, sideC );
+function triangle() {
 var sideA = parseFloat(prompt("Enter the length value of side A here:"));
 var sideB = parseFloat(prompt("Enter the length value of side B here"));
 var sideC = parseFloat(prompt("Enter the length value of side C here"));
-function return [];
 
 if (sideA===sideB && sideB===sideC && sideA===sideC) {
   alert ("This triangle is an equilateral triangle");
-};
+}
 else if (sideA===sideB || sideB===sideC || sideA===sideC) {
   alert ("This triangle is an isosceles triangle");
-};
-else if (sideA + sideB <= sideC || sideB + sideC<=sideA || sideA + sideC<=sideB) {
-  alert ("This is a not a triangle!")
+}else if((sideA!=sideB && sideB!=sideC && sideA!=sideC) && sideA + sideB > sideC || sideB + sideC>sideA || sideA + sideC>sideB){
+  alert ("This is a scalene");
 }
-else {(sideA!sideB && sideB!sideC && sideA!sideC) && sideA + sideB > sideC || sideB + sideC>sideA || sideA + sideC>sideB)
-  alert ("This is a scalene")
+else if (sideA + sideB <= sideC || sideB + sideC<=sideA || sideA + sideC<=sideB) {
+  alert ("This is a not a triangle!");
+}};
